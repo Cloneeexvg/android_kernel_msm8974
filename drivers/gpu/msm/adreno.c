1c68d43f0f0bd072e9bed993679e4f16f0c302f4
Jan Forman <info@janforman.com>
2020-02-24 14:24:46 +0000

    Relax CPU latency requirements to save power
    Change-Id: Id2a2838aa2805a6e07ffcd0c218da1d3aa509e1c

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1730,7 +1730,7 @@ static int adreno_of_get_pdata(struct platform_device *pdev)
 	/* get pm-qos-latency from target, set it to default if not found */
 	if (adreno_of_read_property(pdev->dev.of_node, "qcom,pm-qos-latency",
 		&pdata->pm_qos_latency))
-		pdata->pm_qos_latency = 501;
+		pdata->pm_qos_latency = 1000;
 
 
 	if (adreno_of_read_property(pdev->dev.of_node, "qcom,idle-timeout",
